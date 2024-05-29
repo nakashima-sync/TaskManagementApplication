589ee522be75a8aca553fe26de24fc50eba31b89
nakashima-sync <h.nakashima@sync-web.jp>
2024-05-29 14:05:01 +0900

    Inputの追加とControllerの記述

--- TaskManagementApplication/src/main/java/com/example/demo/dao/Dao.java
+++ TaskManagementApplication/src/main/java/com/example/demo/dao/Dao.java
@@ -1,5 +1,13 @@
 package com.example.demo.dao;
 
-public class Dao {
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
 
+public class Dao {
+	private final JdbcTemplate db;
+
+	@Autowired
+	public Dao(JdbcTemplate db) {
+		this.db = db;
+	}
 }
---
+++ TaskManagementApplication/src/main/java/com/example/demo/input/InputProject.java
@@ -0,0 +1,13 @@
+package com.example.demo.input;
+
+public class InputProject {
+	private String name;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+}

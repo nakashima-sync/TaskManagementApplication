c50fa1919784dd2682045e515859017d10410462
matsumoto-masaki <m.matsumoto@sync-web.jp>
2024-05-29 16:46:19 +0900

    aaa

--- TaskManagementApplication/src/main/java/com/example/demo/controller/Controller.java
+++ TaskManagementApplication/src/main/java/com/example/demo/controller/Controller.java
@@ -4,6 +4,8 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.example.demo.entity.EntProject;
+
 public class Controller {
 	@RequestMapping("/home")
 	public String home(Model model) {
@@ -28,5 +30,17 @@ public class Controller {
 	@RequestMapping("/user")
 	public String user(Model model) {
 		return "user.html";
+	}
+
+	@RequestMapping("/project_add")
+	public String project_add(Model model) {
+		EntProject entproject=new EntProject();
+		entproject.setProject_id(entproject.getProject_id());
+		entproject.setProject_name(entproject.getProject_name());		
+		dao.insertDb(entproject);
+		return "redirect:project";
+	
+		return "home.html";
+
 	}
 }
--- TaskManagementApplication/src/main/java/com/example/demo/entity/EntDepart.java
+++ TaskManagementApplication/src/main/java/com/example/demo/entity/EntDepart.java
@@ -1,37 +1,17 @@
 package com.example.demo.entity;
 
 public class EntDepart {
-<<<<<<< HEAD
 	private int id;
 	private int project_id;
 	private int user_id;
-=======
-    private int id;
-    private int project_id;
-    private int task_id;
 
-    public EntDepart() {
-    }
->>>>>>> branch 'master' of git@github.com:nakashima-sync/TaskManagementApplication.git
-
-<<<<<<< HEAD
 	public EntDepart() {
 	}
 
 	public int getId() {
 		return id;
 	}
-=======
-    public int getId() {
-        return id;
-    }
 
-    public void setId(int id) {
-        this.id = id;
-    }
->>>>>>> branch 'master' of git@github.com:nakashima-sync/TaskManagementApplication.git
-
-<<<<<<< HEAD
 	public void setId(int id) {
 		this.id = id;
 	}
@@ -39,17 +19,7 @@ public class EntDepart {
 	public int getProject_id() {
 		return project_id;
 	}
-=======
-    public int getProject_id() {
-        return project_id;
-    }
 
-    public void setProject_id(int project_id) {
-        this.project_id = project_id;
-    }
->>>>>>> branch 'master' of git@github.com:nakashima-sync/TaskManagementApplication.git
-
-<<<<<<< HEAD
 	public void setProject_id(int project_id) {
 		this.project_id = project_id;
 	}
@@ -61,14 +31,5 @@ public class EntDepart {
 	public void setUser_id(int user_id) {
 		this.user_id = user_id;
 	}
-=======
-    public int getTask_id() {
-        return task_id;
-    }
-
-    public void setTask_id(int task_id) {
-        this.task_id = task_id;
->>>>>>> branch 'master' of git@github.com:nakashima-sync/TaskManagementApplication.git
 
     }
-}
